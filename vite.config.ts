2a0df5fb2596ae26a1e0e9e0560df9cf33031a93
yitang.lh <liheact@gmail.com>
2023-12-24 23:31:51 +0800

    feat: update vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  experimental: {
+      renderBuiltUrl(filename: string, { hostType }: { hostType: 'js' | 'css' | 'html' }) {
+        if (hostType === 'js') {
+          return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` }
+        } else {
+          return { relative: true }
+        }
+      }
+    }
 });
